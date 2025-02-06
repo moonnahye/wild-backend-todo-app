7521b1f0f1ce1b8ad325a0722e223dbe134a86b4
nahye <nnhh.shine@gmail.com>
2025-02-07 08:26:46 +0900

    기본 HTTP 서버 구현

--- app/src/main/java/com/example/demo/App.java
+++ app/src/main/java/com/example/demo/App.java
@@ -1,7 +1,41 @@
 package com.example.demo;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        app.run();
+    }
+
+    public void run() throws IOException {
+        InetSocketAddress address = new InetSocketAddress("localhost", 8080);
+        HttpServer httpServer = HttpServer.create(address, 0);
+        httpServer.createContext("/", exchange -> {
+            URI uri = exchange.getRequestURI();
+            String path = uri.getPath();
+            String method = exchange.getRequestMethod();
+
+            String content = "";
+
+            if(path.equals("/") && method.equals("GET")) {
+                content = "Hello World \n";
+            }
+            if(content.isEmpty()){
+                exchange.sendResponseHeaders(204, 0);
+            }
+            byte[] bytes = content.getBytes();
+            exchange.sendResponseHeaders(200, bytes.length);
+
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(bytes);
+            outputStream.close();
+        });
+        httpServer.start();
     }
 }
